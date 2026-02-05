aa24b0e79428d373e3bd5355cae370d313bdd165
ccrimetz <christian.crimetz@esante.gouv.fr>
2026-02-05 15:15:20 +0100

    feat: ajout du variables.tf

---
+++ ovh/variables.tf
@@ -0,0 +1,17 @@
+variable "s3" {
+  type = object({
+    region = string,
+    replica_region = string
+    bucket_name = string
+    enable_replication = bool
+    enable_versioning = bool
+    enable_encryption = bool
+  })  
+}
+
+variable "ovh" {
+  type = object({
+    endpoint = string,  
+    project_id = string
+  })
+}
