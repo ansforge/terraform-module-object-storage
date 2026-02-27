1a1665a1448fffaa5e8631af2428b13e2c264814
ccrimetz <christian.crimetz@esante.gouv.fr>
2026-02-27 16:21:23 +0100

    fix: bucket_name

--- users.tf
+++ users.tf
@@ -63,8 +63,8 @@ resource "ovh_cloud_project_user_s3_policy" "s3_policies" {
           jsondecode(each.value.custom_policy_json).actions
         ) : local.policy_actions[each.value.policy]
         Resource = [
-          "arn:aws:s3:::${var.bucket_name}",
-          "arn:aws:s3:::${var.bucket_name}/*",
+          "arn:aws:s3:::${var.s3.bucket_name}",
+          "arn:aws:s3:::${var.s3.bucket_name}/*",
         ]
       }
     ]
