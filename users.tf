f0480ed42c5dce384cc7081d90177350520167a0
ccrimetz <christian.crimetz@esante.gouv.fr>
2026-02-27 20:16:40 +0100

    fix: mise à jour des actions

--- users.tf
+++ users.tf
@@ -5,17 +5,16 @@ locals {
     read_only = [
       "s3:GetObject",
       "s3:ListBucket",
-      "s3:GetBucketLocation",
-      "s3:GetObjectVersion",
+      "s3:ListMultipartUploadParts",
+      "s3:ListBucketMultipartUploads",
     ]
     read_write = [
       "s3:GetObject",
       "s3:PutObject",
       "s3:DeleteObject",
       "s3:ListBucket",
-      "s3:GetBucketLocation",
-      "s3:GetObjectVersion",
-      "s3:PutObjectAcl",
+      "s3:ListBucketMultipartUploads",
+      "s3:ListMultipartUploadParts",
     ]
     admin = ["s3:*"]
   }
